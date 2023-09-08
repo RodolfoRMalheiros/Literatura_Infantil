c8df6610c2a823e73cfa3f1999baabe739bc00b5
Rodolfo Malheiros <125034296+RodolfoRMalheiros@users.noreply.github.com>
2023-09-08 15:09:36 -0300

    Site2Literatura

---
+++ Site2Literatura/scripts.js
@@ -0,0 +1,16 @@
+// JavaScript para o carrossel de livros (pode ser implementado com uma biblioteca como Slick Carousel)
+function showBooks() {
+    // Lógica para mostrar os 4 primeiros livros
+}
+
+function showSusanList() {
+    // Lógica para mostrar a lista de Susan Bayer
+}
+
+function showClassicalList() {
+    // Lógica para mostrar a lista do Classical Conversations
+}
+
+function showEnglishLiteratureList() {
+    // Lógica para mostrar a lista de Literatura Inglesa
+}
